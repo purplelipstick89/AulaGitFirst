577691df5a877dd3cc73fad55d455177c132e172
Lary Tertuliano <laryssa.tertuliano@gmail.com>
2019-04-24 14:08:02 -0300

    Initial Commit

---
+++ AulaGitFirst/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AulaGitFirst
+//
+//  Created by Lary Tertuliano on 24/04/19.
+//  Copyright © 2019 Lary Tertuliano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
