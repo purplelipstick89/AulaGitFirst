696b896a52121c05c0129b821ebd06e7080f8061
Lary Tertuliano <laryssa.tertuliano@gmail.com>
2019-04-24 14:45:13 -0300

    adiciona favoritos

---
+++ AulaGitFirst/Favoritos.swift
@@ -0,0 +1,9 @@
+//
+//  Favoritos.swift
+//  AulaGitFirst
+//
+//  Created by Lary Tertuliano on 24/04/19.
+//  Copyright © 2019 Lary Tertuliano. All rights reserved.
+//
+
+import Foundation
--- AulaGitFirst/ViewController.swift
+++ AulaGitFirst/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
 print ("Hello Git <3")
+ 
+print ("Hello Git <3 2")
+        
     }
 
 
